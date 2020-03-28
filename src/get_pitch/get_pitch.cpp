e30c34acf0603206d3709821e795ae464ebb0286
titocampis <61728895+titocampis@users.noreply.github.com>
2020-03-28 12:41:05 +0100

    Add files via upload

--- src/get_pitch/get_pitch.cpp
+++ src/get_pitch/get_pitch.cpp
@@ -4,6 +4,8 @@
 #include <fstream>
 #include <string.h>
 #include <errno.h>
+#include <cmath>
+#include "ffft/FFTReal.h"
 
 #include "wavfile_mono.h"
 #include "pitch_analyzer.h"
@@ -58,13 +60,49 @@ int main(int argc, const char *argv[]) {
   int n_len = rate * FRAME_LEN;
   int n_shift = rate * FRAME_SHIFT;
 
+    /// \TODO
+  /// Preprocess the input signal in order to ease pitch estimation. For instance,
+  /// central-clipping or low pass filtering may be used. ===============================================
+
   // Define analyzer
   PitchAnalyzer analyzer(n_len, rate, PitchAnalyzer::HAMMING, 50, 500);
 
-  /// \TODO
-  /// Preprocess the input signal in order to ease pitch estimation. For instance,
-  /// central-clipping or low pass filtering may be used.
-  
+  // Real FFT
+  ffft::FFTReal <float> fft_object (1024);
+
+  // We make the FFT to x signal
+  std::vector<float> X;
+  std::vector<float> X_filt;
+  std::vector<float> x_filt;
+  X.resize(x.size());
+  fft_object.do_fft(X.data(), x.data()); // X = fft(x)
+
+  //Fistro 2 Paso Bajo siguiendo wikipedia
+  //float alpha = 0.7;
+ // for (unsigned int p = 2; p < x.size(); p++)
+  //  x[p] = alpha*x[p]+(1-alpha)*x[p-1];
+
+  // Fistro Paso Bajo
+/*  std::vector<float> low_pass_filter;
+  low_pass_filter.resize(x.size());
+  float f_cut = 1000; //Threshold frequency
+  int k_cut = (f_cut/rate)*X.size(); //Transform f into k
+
+  // Calculate filter
+  for(int n = 0; n < X.size(); n++) {
+    if(n < k_cut) low_pass_filter[n] = 1;
+    else low_pass_filter[n] = 0;
+  }
+
+  //Filter
+  for(int n = 0; n < X.size(); n++) X_filt[n] = X[n]*low_pass_filter[n];
+
+  // FFT-1
+  fft_object.do_ifft(X_filt.data(), x.data());
+  fft_object.rescale(x.data());*/
+
+  //====================================================================================================
+
   // Iterate for each frame and save values in f0 vector
   vector<float>::iterator iX;
   vector<float> f0;
@@ -76,6 +114,7 @@ int main(int argc, const char *argv[]) {
   /// \TODO
   /// Postprocess the estimation in order to supress errors. For instance, a median filter
   /// or time-warping may be used.
+
 
   // Write f0 contour into the output file
   ofstream os(output_txt);
